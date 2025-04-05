5fa03314414f96e1b5f1073332f805e1d7f806d5
raffkelly <raffkelly@gmail.com>
2025-04-04 23:26:23 -0400

    fixing

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
 
-	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
+	"github.com/raffkelly/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/raffkelly/learn-file-storage-s3-golang-starter/internal/database"
 )
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
@@ -28,8 +31,38 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
+
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "unable to parse data", nil)
+		return
+	}
+	defer file.Close()
+	contentType := header.Header.Get("Content-Type")
+	fileData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, 500, "unable to read file data", nil)
+		return
+	}
+	videoData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, 500, "unable to retrieve video from db", nil)
+		return
+	}
+	if videoData.CreateVideoParams.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "user not authorized to get video data", errors.New("unauthorized user"))
+		return
+	}
+	thumb := thumbnail{
+		data:      fileData,
+		mediaType: contentType,
+	}
+	videoThumbnails[videoID] = thumb
+	newVideo := database.Video{}
+	cfg.db.UpdateVideo(newVideo)
 
 	// TODO: implement the upload here
 
